1eed893f811dcad8c8c17f0858e71e0bd8a0c633
chandler <chandler@chandlerweiner.com>
2018-11-15 16:58:01 -0500

    Trying to get static site to work

--- contents/data/mainwp/backup/dbBackup-wp_ewwwio_images.sql
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>403 Forbidden</title>
-</head><body>
-<h1>Forbidden</h1>
-<p>You don't have permission to access /contents/data/mainwp/backup/dbBackup-wp_ewwwio_images.sql
-on this server.<br />
-</p>
-</body></html>
